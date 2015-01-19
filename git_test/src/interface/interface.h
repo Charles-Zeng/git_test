3d1df794e7be33dc2386b909dfc5748e865a737f
Charles_Zeng <zengchao_cool@126.com>
2015-01-19 17:42:39 +0800

    add person interface

--- git_test/src/interface/interface.h
+++ git_test/src/interface/interface.h
@@ -2,10 +2,15 @@
 #define _INTERFACE_H_
 
 #include <stdint.h>
+#include <string>
 
 namespace interface
 {
-
+	struct Person
+	{
+		std::string name;
+		std::string email;
+	};
 };
 
 #endif // _INTERFACE_H_
